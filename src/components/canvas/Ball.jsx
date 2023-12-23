a568da8ebf7c69f8cfc506fc1eb17506fc7cfcc7
yanghd <yanghd@vt.edu>
2023-12-22 23:35:39 -0500

    style: change the ball color

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -15,12 +15,12 @@ const Ball = (props) => {
 
   const [decal] = useTexture([props.imgUrl]);
 
-  return <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+  return <Float speed={1.15} rotationIntensity={1} floatIntensity={2}>
     <ambientLight intensity={0.25}/>
     <directionalLight position={[0, 0, 0.05]}/>
     <mesh castShadow receiveShadow scale={2.75}>
       <icosahedronGeometry args={[1, 1]}/>
-      <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading/>
+      <meshStandardMaterial color="#CBC3E3" polygonOffset polygonOffsetFactor={-5} flatShading/>
       <Decal map={decal} position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]}/>
     </mesh>
   </Float>;
